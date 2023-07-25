d392d74dabda5fce988cebe016adb7d35d793f9b
munir79 <jakirhossainmunir79@gmail.com>
2023-07-25 13:08:47 +0600

    first commit

---
+++ templeteString.js
@@ -0,0 +1,24 @@
+// 1. let : its re- assaignable 
+// 2.const :  its not re assaignable 
+
+// let a=10;
+// a=20;
+
+// const b=20;
+// b=21; // its not allaow in const variable 
+
+
+//3.templete string ``
+
+const multiline= `my name is 
+jakir hossain
+munir `;
+
+console.log(multiline);
+// 4.dynamic variable
+const d=20;
+const e=30;
+
+console.log(`your summetion in :${d} and ${e} are :${d+e}`);
+
+
